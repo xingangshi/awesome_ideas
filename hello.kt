2c45032e249724bb2d9f1e4be5b1bbfbdfb8d869
shshixingang <shshixingang@corp.netease.com>
2021-02-03 20:21:24 +0800

    some notes just for kotlin

--- hello.kt
+++ hello.kt
@@ -1,5 +1,31 @@
+/**
+  1. Install kotlin for Terminal
+  curl -s https://get.sdkman.io | bash
+  sdk selfupdate force
+
+  sdk install kotlin
+
+  apt install openjdk-11-jdk-headles
+
+
+  2. Run .kt files as an application
+  kotlinc hello.kt -include-runtime -d hello.jar
+  java -jar hello.jar
+
+  3. Using .kt as a lib
+  kotlinc hello.kt -d hello.jar
+  kotlin -classpath hello.jar HelloKt
+
+  4. Run in the REPL
+   $ kotlinc-jvm
+ */
+
+// Oneline comment
+//
+// just a simple kotlin demo
 fun main() {
-  println("What's your name？")
+  println("What's your name ?")
   val name = readLine()
-  println("Hello, $name!")
+  println("Hello, $name !")
 }
+
