79bbd1afb5c64b80a2e2f25bb66323c438ffd57d
shshixingang <shshixingang@corp.netease.com>
2021-02-03 20:33:25 +0800

    some notes just for kotlin

--- hello.kt
+++ hello.kt
@@ -1,23 +1,29 @@
 /**
   1. Install kotlin for Terminal
-  curl -s https://get.sdkman.io | bash
-  sdk selfupdate force
+  1.1 Install Sdk
+      $ curl -s https://get.sdkman.io | bash
 
-  sdk install kotlin
+      Open a new Terminal, then:
+      $ source "/home/root/.sdkman/bin/sdkman-init.sh"
+      $ sdk selfupdate force
 
-  apt install openjdk-11-jdk-headles
+  1.2 Install kotlin
+      $sdk install kotlin
 
+  1.3 Install openjdk
+      $apt-get update
+      $apt-get install -y openjdk-11-jdk-headless
 
   2. Run .kt files as an application
-  kotlinc hello.kt -include-runtime -d hello.jar
-  java -jar hello.jar
+     $ kotlinc hello.kt -include-runtime -d hello.jar
+     $ java -jar hello.jar
 
   3. Using .kt as a lib
-  kotlinc hello.kt -d hello.jar
-  kotlin -classpath hello.jar HelloKt
+     $ kotlinc hello.kt -d hello.jar
+     $ kotlin -classpath hello.jar HelloKt
 
   4. Run in the REPL
-   $ kotlinc-jvm
+     $ kotlinc-jvm
  */
 
 // Oneline comment
