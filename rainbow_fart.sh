6cbe7c8fed108ee62664ba4954d5caedb211e60b
panshi shuo <geekpanshi@gmail.com>
2022-01-01 09:27:34 +0800

    update infos

--- rainbow_fart.sh
+++ rainbow_fart.sh
@@ -1,20 +1,20 @@
 #/usr/bin/bash
 
 if [[ $1 == "" ]]; then
-  name="Panshi"
+  name="飞哥"
 else
   name=$1
 fi
 
-gender="女"
+gender="男"
 if [[ $2 != "" ]]; then
-  gender="男"
+  gender="女"
 fi
 
 echo -e "\n"
 echo -e "\033[31m\t！！！！ $name $gender神 ！！！！\033[0m"
 echo -e "\033[33m\t！！！！ $name 冲啊 ！！！！\033[0m"
-echo -e "\033[36m\t！！！！ $name 狂爱 ！！！！\033[0m"
+echo -e "\033[36m\t！！！！ $name 生二胎  ！！！！\033[0m"
 echo -e "\033[32m\t！！！！ $name 最棒 ！！！！\033[0m"
 echo -e "\033[35m\t！！！！ $name 支持 ！！！！\033[0m"
 echo -e "\033[34m\t！！！！ $name $gender神 ！！！！\033[0m"
